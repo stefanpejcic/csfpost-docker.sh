dfc0a325625ff7e99d882fff387220060435ab20
Stefan Pejcic <stefan@pejcic.rs>
2024-06-12 00:36:37 +0200

    Update standalone_install.sh

--- standalone_install.sh
+++ standalone_install.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
 
+# Install CSF
+wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+rm csf.tgz
+cd csf
+sh install.sh
 
-edit docker daemon json
+# Add csfpost.sh
+cd ..
+rm -rf csf
+cp csfpost.sh /etc/csf/csfpost.sh
 
-```
-"iptables": false
-```
-
-```
-cp csfpost.sh /usr/local/csf/bin/csfpost.sh
-```
-
-
-
-restart both
-
-cp rules from ufw
-stop and dosbale ufw
+# Disable Docker iptables management
+# You need to manually edit the Docker daemon JSON to disable iptables management
+#
+# edit docker daemon json
+#
+# "iptables": false
 
 
-enable csf
+# Read UFW rules and add them to CSF
+ufw status numbered | grep "\[ *[0-9]\+\]" | while read -r line; do
+    rule=$(echo "$line" | awk '{print $2}')
+    csf -a "$rule"
+done
+
+
+# Disable CSF testing mode
+sed -i 's/TESTING = "1"/TESTING = "0"/' /etc/csf/csf.conf
+
+
+# Restart services
+service ufw stop
+service ufw disable
+service docker restart
+csf -r
+
+# Copy files to OpenPanel
+cp csf.py /usr/local/admin/modules/settings/csf.py
+cp csf.html /usr/local/admin/templates/csf.html
+service admin restart
+
+
+# Check status
 
