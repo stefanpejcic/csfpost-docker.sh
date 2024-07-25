0b743d4609813c4e93449c35548b6dad0820024c
Stefan Pejcic <stefan@pejcic.rs>
2024-07-25 21:42:28 +0200

    Update csfpost.sh

--- csfpost.sh
+++ csfpost.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Get all bridge network names from Docker
-docker_bridge_ids=$(docker network ls --filter driver=bridge --format "{{.ID}}")
+docker_bridge_ids=$(/usr/bin/docker network ls --filter driver=bridge --format "{{.ID}}")
 
 output_header='''
 
 echo "[DOCKER] Setting up FW rules."
 
-iptables -N DOCKER
+/usr/sbin/iptables -N DOCKER
 '''
 
 output_footer='''
@@ -20,21 +20,21 @@ output_body=""
 # Template segment for each bridge-subnet pair
 template_segment='''
 # Masquerade outbound connections from containers
-iptables -t nat -A POSTROUTING -s {subnet} ! -o {bridge} -j MASQUERADE
+/usr/sbin/iptables -t nat -A POSTROUTING -s {subnet} ! -o {bridge} -j MASQUERADE
 
 
 # Accept established connections to the docker containers
-iptables -t filter -A FORWARD -o {bridge} -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
+/usr/sbin/iptables -t filter -A FORWARD -o {bridge} -m conntrack --ctstate NEW,RELATED,ESTABLISHED -j ACCEPT
 
 # Allow docker containers to communicate with themselves & outside world
-iptables -t filter -A FORWARD -i {bridge} ! -o {bridge} -j ACCEPT
-iptables -t filter -A FORWARD -i {bridge} -o {bridge} -j ACCEPT
+/usr/sbin/iptables -t filter -A FORWARD -i {bridge} ! -o {bridge} -j ACCEPT
+/usr/sbin/iptables -t filter -A FORWARD -i {bridge} -o {bridge} -j ACCEPT
 '''
 
 # Loop through each network ID and inspect it
 for id in $docker_bridge_ids; do
     # Extract the bridge name (network interface) using docker network inspect
-    bridge_name=$(docker network inspect $id --format '{{ index .Options "com.docker.network.bridge.name" }}')
+    bridge_name=$(/usr/bin/docker network inspect $id --format '{{ index .Options "com.docker.network.bridge.name" }}')
     # Check if the bridge name is not empty
     if [ -n "$bridge_name" ]; then
         bridge_name="$bridge_name"
@@ -42,8 +42,8 @@ for id in $docker_bridge_ids; do
         # Fallback to default bridge naming convention if custom name isn't set
         bridge_name="br-$id"
     fi
-    subnet=$(docker network inspect $id --format '{{(index .IPAM.Config 0).Subnet}}')
-    
+    subnet=$(/usr/bin/docker network inspect $id --format '{{(index .IPAM.Config 0).Subnet}}')
+
     # Substitute the bridge and subnet into the template segment
     if [ -n "$subnet" ]; then
         output_body+="$(echo "$template_segment" | sed "s|{subnet}|$subnet|g" | sed "s|{bridge}|$bridge_name|g")"
